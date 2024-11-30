071b78a54e8ddfb7f8a37ef859f70099d4eafb5a
107422011+RadekHerman@users.noreply.github.com <107422011+RadekHerman@users.noreply.github.com>
2024-11-30 15:12:27 +0100

    feat: zwinny power block removed

--- OOPpractice2/Hero_Zwinny.cs
+++ OOPpractice2/Hero_Zwinny.cs
@@ -4,10 +4,10 @@
 namespace OOPpractice2
 {
     
-    public class Hero_Zwinny : Hero, IPowerHit, IPowerBlock
+    public class Hero_Zwinny : Hero, IPowerHit 
     {
         static private Random random = new Random();
-        public Hero_Zwinny(string name = "Zwinny", int health = 100, int maxAttack = 26, int maxBlock = 151, int unitHealth = 100)
+        public Hero_Zwinny(string name = "Zwinny", int health = 100, int maxAttack = 26, int maxBlock = 201, int unitHealth = 100)
             : base(name, health, maxAttack, maxBlock, unitHealth)
         { 
         }
@@ -17,22 +17,10 @@ namespace OOPpractice2
             int rnd = random.Next(0, MaxAttack);
             if ((rnd > 9) && (rnd < 16))
             {
-                Console.WriteLine($" {this.Name} PowerHIT {rnd * 4}"); 
+                Console.WriteLine($" {this.Name} PowerHIT {rnd * 4}");
                 return (rnd * 4);
             }
             else return rnd;
-        }
-
-        public int PowerBlock()
-        {
-            int rnd = random.Next(0, MaxBlock);
-            if ((rnd > 130) && (rnd < 10))
-            {
-                Console.WriteLine($" {this.Name} PowerBlock 1000 ");
-                return 1000;
-            }
-            else return rnd;
-            
         }
 
     }
